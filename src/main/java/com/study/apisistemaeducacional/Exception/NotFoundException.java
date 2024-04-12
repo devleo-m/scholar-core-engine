6ced383c6ef427595b800ab777724614247f77d9
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-12 18:39:19 -0300

    NotFoundException criado

---
+++ src/main/java/com/study/apisistemaeducacional/Exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.study.apisistemaeducacional.Exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
