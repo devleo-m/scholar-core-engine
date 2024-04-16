d2bdfb3e45830f6859d16c1a5f87b5a9d673130c
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-16 20:19:55 -0300

    Atualizando o metodo criardocente com o dto

--- src/main/java/com/study/apisistemaeducacional/Service/DocenteService.java
+++ src/main/java/com/study/apisistemaeducacional/Service/DocenteService.java
@@ -1,12 +1,14 @@
 package com.study.apisistemaeducacional.Service;
 
+import com.study.apisistemaeducacional.Controller.dto.request.CriarDocenteRequest;
+import com.study.apisistemaeducacional.Controller.dto.response.CriarDocenteResponse;
 import com.study.apisistemaeducacional.Entity.DocenteEntity;
 
 import java.util.List;
 
 public interface DocenteService {
-    DocenteEntity criarDocente(DocenteEntity docente);
-    DocenteEntity obterDocentePorId(Long id);
+    CriarDocenteResponse criarDocente(CriarDocenteRequest request);
+    public CriarDocenteResponse obterDocentePorId(Long id);
     DocenteEntity atualizarDocente(Long id, DocenteEntity docente);
     List<DocenteEntity> listarTodosDocentes();
     void deletarDocente(Long id);
