494c20c9a523a8f680f04d01aaf5a6a6258a8e8c
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-17 03:17:51 -0300

    Atualizando os metodos

--- src/main/java/com/study/apisistemaeducacional/Service/DocenteService.java
+++ src/main/java/com/study/apisistemaeducacional/Service/DocenteService.java
@@ -1,15 +1,15 @@
 package com.study.apisistemaeducacional.Service;
 
-import com.study.apisistemaeducacional.Controller.dto.request.CriarDocenteRequest;
-import com.study.apisistemaeducacional.Controller.dto.response.CriarDocenteResponse;
+import com.study.apisistemaeducacional.Controller.dto.request.DocenteRequest;
+import com.study.apisistemaeducacional.Controller.dto.response.DocenteResponse;
 import com.study.apisistemaeducacional.Entity.DocenteEntity;
 
 import java.util.List;
 
 public interface DocenteService {
-    CriarDocenteResponse criarDocente(CriarDocenteRequest request);
-    CriarDocenteResponse obterDocentePorId(Long id);
-    DocenteEntity atualizarDocente(Long id, DocenteEntity docente);
-    List<CriarDocenteResponse> listarTodosDocentes();
+    DocenteResponse criarDocente(DocenteRequest request);
+    DocenteResponse obterDocentePorId(Long id);
+    DocenteEntity atualizarDocente(Long id, DocenteRequest request);
+    List<DocenteResponse> listarTodosDocentes();
     void deletarDocente(Long id);
 }
