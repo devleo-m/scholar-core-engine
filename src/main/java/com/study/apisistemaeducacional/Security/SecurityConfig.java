9641be42107ebc7c078e1a77266a85c792b7e0f8
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-18 23:44:29 -0300

    Update SecurityConfig - Add permissoes request

--- src/main/java/com/study/apisistemaeducacional/Security/SecurityConfig.java
+++ src/main/java/com/study/apisistemaeducacional/Security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.study.apisistemaeducacional.Security;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -27,25 +29,67 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.info("Configurando a cadeia de filtros de segurança");
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        // Permissão pública
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/registrar").permitAll()
+
+                        // Permissões DOCENTE
+                        .requestMatchers(HttpMethod.GET, "/api/docentes/**").hasAnyRole("ADMIN", "PEDAGOGICO", "RECRUITER")
+                        .requestMatchers(HttpMethod.POST, "/api/docentes/**").hasAnyRole("ADMIN", "PEDAGOGICO", "RECRUITER")
+                        .requestMatchers(HttpMethod.PUT, "/api/docentes/**").hasAnyRole("ADMIN", "PEDAGOGICO", "RECRUITER")
+
+                        // Permissões ALUNO
+                        .requestMatchers(HttpMethod.GET, "/api/alunos/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+                        .requestMatchers(HttpMethod.POST, "/api/alunos/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+                        .requestMatchers(HttpMethod.PUT, "/api/alunos/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+
+                        // Permissões CURSO
+                        .requestMatchers(HttpMethod.GET, "/api/cursos/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+                        .requestMatchers(HttpMethod.POST, "/api/cursos/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+                        .requestMatchers(HttpMethod.PUT, "/api/cursos/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+
+                        // Permissões MATERIA
+                        .requestMatchers(HttpMethod.GET, "/api/materias/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+                        .requestMatchers(HttpMethod.POST, "/api/materias/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+                        .requestMatchers(HttpMethod.PUT, "/api/materias/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+
+                        // Permissões TURMA
+                        .requestMatchers(HttpMethod.GET, "/api/turmas/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+                        .requestMatchers(HttpMethod.POST, "/api/turmas/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+                        .requestMatchers(HttpMethod.PUT, "/api/turmas/**").hasAnyRole("ADMIN", "PEDAGOGICO")
+
+                        // Permissões NOTA
+                        .requestMatchers(HttpMethod.GET, "/api/notas/**").hasAnyRole("ADMIN", "PROFESSOR")
+                        .requestMatchers(HttpMethod.POST, "/api/notas/**").hasAnyRole("ADMIN", "PROFESSOR")
+                        .requestMatchers(HttpMethod.PUT, "/api/notas/**").hasAnyRole("ADMIN", "PROFESSOR")
+                        .requestMatchers(HttpMethod.GET, "/api/notas/aluno/{id}").hasRole("ALUNO")
+                        .requestMatchers(HttpMethod.GET, "/api/notas/aluno/{id}/pontuacao").hasRole("ALUNO")
+
+                        // Permissões especificas do ADMIN
+                        .requestMatchers(HttpMethod.POST, "/registrar").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+
+                        // Todas as outras requisições autenticadas
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        log.info("Cadeia de filtros de segurança configurada");
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+        log.info("Configurando a cadeia de password encoder");
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        log.info("Configurando a cadeia de authentication manager");
         return authenticationConfiguration.getAuthenticationManager();
     }
 }
