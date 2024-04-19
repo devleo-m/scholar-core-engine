edce9a392221c83e27e547ddab4c7b4ff9c563a7
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-18 23:44:58 -0300

    Update - correcao de bug e add roles

--- src/main/java/com/study/apisistemaeducacional/Security/CustomUserDetailsService.java
+++ src/main/java/com/study/apisistemaeducacional/Security/CustomUserDetailsService.java
@@ -2,21 +2,30 @@ package com.study.apisistemaeducacional.Security;
 
 import com.study.apisistemaeducacional.Entity.UsuarioEntity;
 import com.study.apisistemaeducacional.Repository.UsuarioRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.List;
 
+@Slf4j
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UsuarioRepository repository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UsuarioEntity user = this.repository.findByLogin(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getSenha(), new ArrayList<>());
+        log.info("Carregando usuário pelo nome de usuário: {}", username);
+        UsuarioEntity user = this.repository.findByLogin(username).orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado! :("));
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getPapel().getNome().toUpperCase()));
+        log.info("Usuário carregado: {}", user.getLogin());
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getSenha(), authorities);
     }
 }
