d70c5d0803ec3877d3bfb4c9703f746ad16b3570
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-20 01:45:44 -0300

    NotaServiceImpl update listarNotaDoAluno e calcularNotaTotalAluno

--- src/main/java/com/study/apisistemaeducacional/Service/Impl/NotaServiceImpl.java
+++ src/main/java/com/study/apisistemaeducacional/Service/Impl/NotaServiceImpl.java
@@ -7,6 +7,7 @@ import com.study.apisistemaeducacional.Controller.dto.response.NotaTotalResponse
 import com.study.apisistemaeducacional.Entity.*;
 import com.study.apisistemaeducacional.Exception.NotFoundException;
 import com.study.apisistemaeducacional.Repository.*;
+import com.study.apisistemaeducacional.Security.TokenService;
 import com.study.apisistemaeducacional.Service.NotaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +27,7 @@ public class NotaServiceImpl implements NotaService {
     private final AlunoRepository alunoRepository;
     private final DocenteRepository docenteRepository;
     private final MateriaRepository materiaRepository;
+    private final TokenService tokenService;
 
     /**
      * Lista todas as notas associadas a um aluno específico.
@@ -241,14 +243,17 @@ public class NotaServiceImpl implements NotaService {
     /*
     * Metodo exclusivo para ALUNO verificar a nota total!
     */
-    public NotaTotalResponse calcularNotaTotalAluno(Long idAluno) {
+    public NotaTotalResponse calcularNotaTotalAluno(String token) {
+        String login = tokenService.validateToken(token); // token para obter o login do aluno
+
+        AlunoEntity aluno = alunoRepository.findByUsuarioLogin(login)
+                .orElseThrow(() -> new RuntimeException("Aluno não encontrado."));
+        Long idAluno = aluno.getId();
+
         List<NotaEntity> notas = notaRepository.findAllByIdAluno(idAluno);
         Double somaNotas = notas.stream()
                 .mapToDouble(NotaEntity::getValor)
                 .sum();
-
-        AlunoEntity aluno = alunoRepository.findById(idAluno)
-                .orElseThrow(() -> new RuntimeException("Aluno não encontrado."));
 
         Long quantidadeMaterias = notaRepository.countMateriasByAlunoId(idAluno);
 
@@ -257,7 +262,6 @@ public class NotaServiceImpl implements NotaService {
         }
 
         Double notaTotal = (somaNotas / quantidadeMaterias) * 10;
-
         return new NotaTotalResponse(
                 aluno.getId(),
                 aluno.getNome(),
@@ -266,13 +270,19 @@ public class NotaServiceImpl implements NotaService {
     }
 
     @Override
-    public List<NotaPorAlunoResponse> listarNotaDoAluno(Long id) {
-        log.info("Listando todas as notas para o aluno com o ID: {}", id);
-        List<NotaEntity> notas = notaRepository.findAllByIdAluno(id);
+    public List<NotaPorAlunoResponse> listarNotaDoAluno(String token) {
+        String login = tokenService.validateToken(token);
+
+        AlunoEntity aluno = alunoRepository.findByUsuarioLogin(login)
+                .orElseThrow(() -> new RuntimeException("Aluno não encontrado."));
+        Long idAluno = aluno.getId();
+
+        log.info("Listando todas as notas para o aluno com o ID: {}", idAluno);
+        List<NotaEntity> notas = notaRepository.findAllByIdAluno(idAluno);
 
         if (notas.isEmpty()) {
-            log.warn("Nenhuma nota encontrada! Aguarde seu professor lancar ela");
-            throw new NotFoundException("Nenhuma nota encontrada! Aguarde seu professor lancar ela");
+            log.warn("Nenhuma nota encontrada! Aguarde seu professor lançar ela");
+            throw new NotFoundException("Nenhuma nota encontrada! Aguarde seu professor lançar ela");
         }
 
         return notas.stream()
