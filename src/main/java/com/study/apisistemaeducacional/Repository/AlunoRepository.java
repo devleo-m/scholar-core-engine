f7b8d9623297b8689b889218ed1d2658f99db705
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-20 01:39:41 -0300

    AlunoRepository - findByUsuario

--- src/main/java/com/study/apisistemaeducacional/Repository/AlunoRepository.java
+++ src/main/java/com/study/apisistemaeducacional/Repository/AlunoRepository.java
@@ -1,9 +1,14 @@
 package com.study.apisistemaeducacional.Repository;
 
 import com.study.apisistemaeducacional.Entity.AlunoEntity;
+import com.study.apisistemaeducacional.Entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AlunoRepository extends JpaRepository<AlunoEntity, Long> {
+    Optional<AlunoEntity> findByUsuario(UsuarioEntity usuario);
+    Optional<AlunoEntity> findByUsuarioLogin(String usuario);
 }
