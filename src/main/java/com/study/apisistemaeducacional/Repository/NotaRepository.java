7ca16de6446c01d23c8085f6792046df20f4a15d
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-19 22:24:42 -0300

    NotaRepository update- Query's

--- src/main/java/com/study/apisistemaeducacional/Repository/NotaRepository.java
+++ src/main/java/com/study/apisistemaeducacional/Repository/NotaRepository.java
@@ -3,6 +3,7 @@ package com.study.apisistemaeducacional.Repository;
 import com.study.apisistemaeducacional.Entity.NotaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,8 @@ import java.util.List;
 public interface NotaRepository extends JpaRepository<NotaEntity, Long> {
     @Query("SELECT nota FROM NotaEntity nota WHERE nota.id_aluno.id = :idAluno")
     List<NotaEntity> findAllByIdAluno(Long idAluno);
+
+    @Query("SELECT COUNT(DISTINCT n.id_materia.id) FROM NotaEntity n WHERE n.id_aluno.id = :idAluno")
+    Long countMateriasByAlunoId(@Param("idAluno") Long idAluno);
+
 }
